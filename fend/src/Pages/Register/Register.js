a043fb35d4a60b1ceb9225b5b7b2eaa0fe0a0da3
Vijay <rebalvijay930@gmail.com>
2025-01-25 10:34:24 +0530

    updated

--- fend/src/Pages/Register/Register.js
+++ fend/src/Pages/Register/Register.js
@@ -17,12 +17,6 @@ function Register() {
           username,
           email,
           password,
-        },
-        {
-          withCredentials:true,
-          headers: {
-            'Content-Type': 'application/json',
-        },
         }
       );
       console.log(response?.data?.message);
